f5a9c56e75e08a54e486d67748b788ab42292f97
CaioRodriguesOliveira <126218519+CaioRodriguesOliveira@users.noreply.github.com>
2023-06-21 12:28:43 -0300

    att

---
+++ basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo">Olá PHP</div>
+
+<?php 
+echo 'Olá <br>';
+echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+
+<?="Outra forma de me 'expressar'! " ?>
+<?= 2*3+3 ?>
+
+<?php
+phpinfo();
